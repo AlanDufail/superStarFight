a08bc47960ef838b2743f43e0c566865a0148b97
AlanDufail <alan.dufail@mail-ecv.fr>
2022-06-08 21:32:19 +0200

    adding routes sheets

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AttaqueController;
+use App\Http\Controllers\Admin\IndexAdminController;
+use App\Http\Controllers\Player\IndexPlayerController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
+
+Route::get('/characterCreation',
+    'App\Http\Controllers\CharacterController@createUserForm'
+)->middleware(['auth'])->name('form-inscription');
+Route::post('/characterCreation', 'App\Http\Controllers\CharacterController@store');
+
+
+
+
+Route::get('/attaqueCreation',
+    'App\Http\Controllers\AttaqueController@createUserForm'
+)->middleware(['auth'])->name('form-attaque');
+Route::post('/form-attaque', 'App\Http\Controllers\AttaqueController@store');
+
+
+
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    /**
+     * Route landing page
+     */
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::middleware(['auth', 'role:admin'])->name('admin.')->prefix('admin')->group(function () {
+        Route::get('/', [IndexAdminController::class, 'index'])->name('index');
+        Route::resource('/roles', RoleController::class);
+        Route::post('/roles/{role}/permissions', [RoleController::class, 'givePermission'])->name('roles.permissions');
+        Route::delete('/roles/{role}/permissions/{permission}', [RoleController::class, 'revokePermission'])->name('roles.permissions.revoke');
+        Route::resource('/permissions', PermissionController::class);
+        Route::post('/permissions/{permission}/roles', [PermissionController::class, 'assignRole'])->name('permissions.roles');
+        Route::delete('/permissions/{permission}/roles/{role}', [PermissionController::class, 'removeRole'])->name('permissions.roles.remove');
+        Route::get('/users', [UserController::class, 'index'])->name('users.index');
+        Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
+        Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+        Route::post('/users/{user}/roles', [UserController::class, 'assignRole'])->name('users.roles');
+        Route::delete('/users/{user}/roles/{role}', [UserController::class, 'removeRole'])->name('users.roles.remove');
+        Route::post('/users/{user}/permissions', [UserController::class, 'givePermission'])->name('users.permissions');
+        Route::delete('/users/{user}/permissions/{permission}', [UserController::class, 'revokePermission'])->name('users.permissions.revoke');
+    });
+
+
+    Route::middleware(['auth', 'role:player'])->name('player.')->prefix('player')->group(function () {
+        Route::get('/', [IndexPlayerController::class, 'index'])->name('index');
+    });
+
 });
+
+require __DIR__ . '/auth.php';
